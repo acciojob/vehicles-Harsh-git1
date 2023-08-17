a20eda472a16232ff9c1777567322b1ca57b6e1b
Harsh Srivastava <srivastavaharsh8957@gmail.com>
2023-08-17 17:14:30 +0530

    vehicles sol

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,12 +2,21 @@ package com.driver;
 
 public class F1 extends Car {
 
+
+    public F1(String name, int wheels, String type, int doors, int gears, boolean isManual, int currentGear, int seats) {
+        super(name, wheels, type, doors, gears, isManual, currentGear, seats);
+    }
+
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name, 4, "f1", 4, 4, isManual, 0, 6);
     }
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        newSpeed = getCurrentSpeed() + rate;
+        int gear = 1;
+
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -18,13 +27,22 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+        if(newSpeed >= 51 && newSpeed <= 100) gear = 2;
+        else if(newSpeed >= 101 && newSpeed <= 150) gear = 3;
+        else if(newSpeed >= 151 && newSpeed <= 200) gear = 4;
+        else if(newSpeed >= 201 && newSpeed <= 250) gear = 5;
+        else gear = 6;
+
+        if(newSpeed <= 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
         }
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
+            changeGear(gear);
         }
     }
 }
--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -1,6 +1,11 @@
 package com.driver;
 
 public interface WaterVehicle {
+
+
     public String getVehicleName();
     public int getVehicleCapacity();
 }
+
+
+
